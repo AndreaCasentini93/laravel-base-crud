c3d957fc9513de73871d14812ee78565caf1e191
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-07-19 13:37:02 +0200

    Seeder

---
+++ database/seeds/ComicsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comics;
+use Illuminate\Support\Str;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comicsArray = config('comics');
+        foreach ($comicsArray as $item) {
+            
+            $comics = new Comics();
+
+            $comics->title = $item['title'];
+            $comics->description = $item['description'];
+            $comics->thumb = $item['thumb'];
+            $comics->price = $item['price'];
+            $comics->series = $item['series'];
+            $comics->slug = Str::slug($item['series'], '-');
+            $comics->sale_date = $item['sale_date'];
+            $comics->type = $item['type'];
+
+            $comics->save();
+
+        }
+    }
+}
